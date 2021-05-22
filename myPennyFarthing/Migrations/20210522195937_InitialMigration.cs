0f844f9184f35f3ad25593bcc93c065d83c15a6a
Ryan Hamilton <ryan.l.hamilton11@outlook.com>
2021-05-22 14:03:51 -0600

    Updated Database & Migrations.

---
+++ myPennyFarthing/Migrations/20210522195937_InitialMigration.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace myPennyFarthing.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "myPennyFarthing");
+
+            migrationBuilder.CreateTable(
+                name: "Bike",
+                schema: "myPennyFarthing",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Make = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bike", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                schema: "myPennyFarthing",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Maintenance",
+                schema: "myPennyFarthing",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mileage = table.Column<float>(type: "float(7)", nullable: false),
+                    Cost = table.Column<float>(type: "float(7)", nullable: false),
+                    BikeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Maintenance", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Maintenance_Bike_BikeId",
+                        column: x => x.BikeId,
+                        principalSchema: "myPennyFarthing",
+                        principalTable: "Bike",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ride",
+                schema: "myPennyFarthing",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Distance = table.Column<float>(type: "float(5)", nullable: false),
+                    AvgCadence = table.Column<float>(type: "real", nullable: false),
+                    AvgHR = table.Column<int>(type: "int", nullable: false),
+                    AvgSpeed = table.Column<float>(type: "float(3)", nullable: false),
+                    Ascent = table.Column<int>(type: "int", nullable: false),
+                    Descent = table.Column<int>(type: "int", nullable: false),
+                    HighGrade = table.Column<float>(type: "float(3)", nullable: false),
+                    LowGrade = table.Column<float>(type: "float(3)", nullable: false),
+                    BikeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ride", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ride_Bike_BikeId",
+                        column: x => x.BikeId,
+                        principalSchema: "myPennyFarthing",
+                        principalTable: "Bike",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Maintenance_BikeId",
+                schema: "myPennyFarthing",
+                table: "Maintenance",
+                column: "BikeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ride_BikeId",
+                schema: "myPennyFarthing",
+                table: "Ride",
+                column: "BikeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_UserName",
+                schema: "myPennyFarthing",
+                table: "User",
+                column: "UserName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Maintenance",
+                schema: "myPennyFarthing");
+
+            migrationBuilder.DropTable(
+                name: "Ride",
+                schema: "myPennyFarthing");
+
+            migrationBuilder.DropTable(
+                name: "User",
+                schema: "myPennyFarthing");
+
+            migrationBuilder.DropTable(
+                name: "Bike",
+                schema: "myPennyFarthing");
+        }
+    }
+}
---
+++ myPennyFarthing/Models/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace myPennyFarthing.Models
+{
+    public class AppDbContext : DbContext
+    {
+        //   F I E L D S   &   P R O P E R T I E S
+        public DbSet<Bike> Bikes { get; set; }
+        public DbSet<MX> MXs { get; set; }
+        public DbSet<Ride> Rides { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+
+        //   C O N S T R U C T O R S
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        //   M E T H O D S
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            //   U S E R S
+            modelBuilder.Entity<User>().HasIndex(u => u.UserName).IsUnique();
+
+            //   B I K E S
+
+
+            //   M A I N T E N A N C E    A C T I O N S
+
+
+            //   R I D E S
+        }
+    }
+}
